8aed02c23fdcf7f06441668ca7d04e5bd759d56f
ThienDoNgoc <haithaobathientuthanh@gmail.com>
2024-02-26 21:07:53 +0700

    add feed carrot

---
+++ Assets/Script/Carrot.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Carrot : MonoBehaviour
+{
+    [SerializeField] private int value;
+    private bool hasTriggered;
+    private CarrotManager carrotManager;
+    private void Start()
+    {
+        if (CarrotManager.instance != null)
+        {
+            carrotManager = CarrotManager.instance;
+        }
+        else
+        {
+            Debug.LogError("CarrotManager instance is null.");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")&& !hasTriggered)
+        {
+            hasTriggered = true;
+            carrotManager.ChangCarrots(value);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/CarrotManager.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class CarrotManager : MonoBehaviour
+{
+    public static CarrotManager instance;
+    private int carrots;
+    [SerializeField] private TMP_Text carrotDisplay;
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+    }
+    private void OnGUI()
+    {
+        carrotDisplay.text = carrots.ToString();
+    }
+    public void ChangCarrots(int amount)
+    {
+        carrots += amount;
+    }
+}
